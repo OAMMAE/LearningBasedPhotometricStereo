56c4538ddb7495a432501faaccf2cc43a5fa0152
Ammae <ammae.osamu@ist.osaka-u.ac.jp>
2017-01-04 19:19:13 +0000

    sh update

--- LearningBasedPhotometricStereo/execution.sh
+++ LearningBasedPhotometricStereo/execution.sh
@@ -1,3 +1,6 @@
-./build/helloworld /mnt/poplin/tmp/ammae/data/ 8
+for i in `seq 0 49`
+do
+  ./build/helloworld /mnt/poplin/tmp/ammae/data/ 12 $i
+done
 
 sh ./ifttt.sh program_finished. learning owari
